9002797f607018d1ff4220d634f4b39c53a76da8
KShivendu <shivendu@iitbhilai.ac.in>
2021-02-10 15:21:49 +0530

    Production mode env variable

--- cam_recorder/settings.py
+++ cam_recorder/settings.py
@@ -56,6 +56,15 @@ MIDDLEWARE = [
 # CELERY STUFF
 BROKER_URL = 'redis://localhost:6379'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+
+
+import os
+PROD = os.environ['PROD']
+
+if PROD:
+    BROKER_URL = 'redis://redis:6379'
+    CELERY_RESULT_BACKEND = 'redis://redis:6379'
+
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
